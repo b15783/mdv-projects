090cd5914822c582155d40a78f612d4a4120d2cd
Craig Schultz <caschultz2005@gmail.com>
2013-03-12 13:01:15 -0700

    Procedure finished
    Commit updated after the procedure requirement is working properly

--- sdi-project2/schultz_craig_assignment2.js
+++ sdi-project2/schultz_craig_assignment2.js
@@ -1,1 +1,31 @@
-alert("JavaScript works!");
+// Craig A. Schultz
+// Date:  March 10, 2013
+// Project 2
+// SDI 1303
+
+alert("Dont forget to open you console so you can see the output");
+
+// --VARIABLES--
+
+// PROCEDURE
+
+var dayOrNight = function(){
+	var currentHour = (new Date()).getHours(), timeOfDay;
+	if (7 < currentHour < 19) {timeOfDay = "bright"; }
+	else {timeOfDay = "dark"; }
+	
+	console.log("You wake up, and the first thing you realize is that is " + timeOfDay + 
+	" all around you!");
+};
+
+// BOOLEAN FUNCTION
+
+// NUMBER FUNCTION
+ 
+// STRING FUNCTION
+
+// ARRAY FUNCTION
+
+// MAIN SECTION
+
+dayOrNight();
