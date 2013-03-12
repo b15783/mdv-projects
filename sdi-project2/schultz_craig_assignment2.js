401489caa73acd6db61ec4dbf44d876d36ac9355
Craig Schultz <caschultz2005@gmail.com>
2013-03-12 13:24:55 -0700

    Boolean completed
    Saving canges after the boolean function is confirmed to be working

--- sdi-project2/schultz_craig_assignment2.js
+++ sdi-project2/schultz_craig_assignment2.js
@@ -2,14 +2,20 @@
 // Date:  March 10, 2013
 // Project 2
 // SDI 1303
+// Waking up an a deserted island
 
 alert("Dont forget to open you console so you can see the output");
 
 // --VARIABLES--
 
+
+
+;
+
+
 // PROCEDURE
 
-var dayOrNight = function(){
+var getDayOrNight = function(){
 	var currentHour = (new Date()).getHours(), timeOfDay;
 	if (7 < currentHour < 19) {timeOfDay = "bright"; }
 	else {timeOfDay = "dark"; }
@@ -20,7 +26,14 @@ var dayOrNight = function(){
 
 // BOOLEAN FUNCTION
 
+var isHeCold = function(shoes, shirt){
+	if (shoes || shirt){
+		return("warm");}
+	else {return("freezing");}
+};	
+
 // NUMBER FUNCTION
+ 
  
 // STRING FUNCTION
 
@@ -28,4 +41,12 @@ var dayOrNight = function(){
 
 // MAIN SECTION
 
-dayOrNight();
+getDayOrNight();
+
+var hotOrCold = isHeCold(true, true);
+	console.log("The next thing that you realize is that you are " + hotOrCold + ".");
+	
+//console.log("The next thing that you realize is that there are dead bodies all around you!");
+
+	
+	
