2375c5383afbafed609988ef4886a5ae6d1ca651
Craig Schultz <caschultz2005@gmail.com>
2013-03-20 22:39:49 -0700

    Fixed a space error in the log

--- sdi-project3/Schultz_craig_assignment3.js
+++ sdi-project3/Schultz_craig_assignment3.js
@@ -44,12 +44,12 @@ banana.summary();
 console.log("The stranger says, 'I'm very hungry, may I have a banana?");
 var foodReply = mainChar.giveFood();
 console.log(foodReply);
-
+console.log("I wonder if we are the only survivors on the island, lets see if we can find some others.");
 console.log("After searching for casualties, you find the following ones:");
 var reportCasualties = function(json) {
 	for (var i = 0; i < json.casualties.length; i++){
 		var casualty = json.casualties[i];
-		console.log(casualty.name + "is a casualty.  He was " + casualty.age);
+		console.log(casualty.name + " is a casualty.  He was " + casualty.age);
 	};
 };
 
