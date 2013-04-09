d71a6b0125950603723adbcb342e03e35c9dba24
Craig Schultz <caschultz2005@gmail.com>
2013-04-08 21:50:37 -0700

    Update

---
+++ VF/schultz_craig_project1/js/main.js
@@ -0,0 +1,13 @@
+// Craig Schultz
+// Date:  April 8, 2013
+// Project 1
+// VFW 1304
+
+
+element.onclick = displayRestult();
+
+function displayResult () {
+//show(document.body.getElementsByTagName("petDetails")[0]);
+return;
+
+}
--- VF/schultz_craig_project1/main.js
+++
@@ -1,5 +0,0 @@
-// Craig Schultz
-// Date:  April 8, 2013
-// Project 1
-// VFW 1304
-
